4cb34abf5f0e5526f64b7b2a52d2f47d2973586b
triaxx <triaxx@pkgsrc.org>
2019-07-03 06:09:22 +0000

    dovecot2: update blk3 to follow gnutls disabling
    Do not bump revision since binary cannot be altered
    
    pkgsrc changes:
    ---------------
      * make blk3 conform to options.mk
      * move BUILD_DEFS (pkglint WARN--)
      * comment an explicit patch (pkglint ERROR--)

--- mail/dovecot2/buildlink3.mk
+++ mail/dovecot2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2018/11/30 18:43:09 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2019/07/03 06:09:22 triaxx Exp $
 
 BUILDLINK_TREE+=	dovecot
 
@@ -14,8 +14,6 @@ pkgbase:=	dovecot
 .include "../../mk/pkg-build-options.mk"
 .if !empty(PKG_BUILD_OPTIONS.dovecot:Mssl)
 .  include "../../security/openssl/buildlink3.mk"
-.elif !empty(PKG_BUILD_OPTIONS.dovecot:Mgnutls)
-.  include "../../security/gnutls/buildlink3.mk"
 .endif
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
