b98b86f41e5110f9f70bac69616fabadab51844e
Jonathan Perkin <jperkin@joyent.com>
2019-07-04 18:35:53 +0100

    Add mysql80-{client,server} packages for testing.

---
+++ databases/mysql80-client/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.3 2019/02/05 20:19:55 adam Exp $
+
+BUILDLINK_TREE+=	mysql-client
+
+.if !defined(MYSQL_CLIENT_BUILDLINK3_MK)
+MYSQL_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=8.0<8.1
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=8.0<8.1
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql80-client
+BUILDLINK_INCDIRS.mysql-client?=	include/mysql
+BUILDLINK_LIBDIRS.mysql-client?=	lib
+
+.include "../../devel/libevent/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif # MYSQL_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mysql-client
