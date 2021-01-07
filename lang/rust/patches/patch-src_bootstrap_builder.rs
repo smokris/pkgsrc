11e759b4ad01378a11524f6be9f92964341bd4d3
Jonathan Perkin <jperkin@joyent.com>
2021-01-07 09:44:04 +0000

    rust: Backport 1.49.0 update from wip.

--- lang/rust/patches/patch-src_bootstrap_builder.rs
+++ lang/rust/patches/patch-src_bootstrap_builder.rs
@@ -5,17 +5,17 @@ Use @PREFIX@, not $ORIGIN in rpath.
 
 Fix RPATH for pkgsrc.
 
---- src/bootstrap/builder.rs.orig	2020-01-27 15:34:02.000000000 +0000
+--- src/bootstrap/builder.rs.orig	2020-12-29 03:03:09.000000000 +0000
 +++ src/bootstrap/builder.rs
-@@ -454,7 +454,6 @@ impl<'a> Builder<'a> {
+@@ -480,7 +480,6 @@ impl<'a> Builder<'a> {
                  install::Clippy,
                  install::Miri,
                  install::Analysis,
 -                install::Src,
                  install::Rustc
              ),
-             Kind::Run => describe!(run::ExpandYamlAnchors,),
-@@ -956,7 +955,7 @@ impl<'a> Builder<'a> {
+             Kind::Run => describe!(run::ExpandYamlAnchors, run::BuildManifest),
+@@ -1065,7 +1064,7 @@ impl<'a> Builder<'a> {
                  rustflags.arg("-Zosx-rpath-install-name");
                  Some("-Wl,-rpath,@loader_path/../lib")
              } else if !target.contains("windows") {
--- lang/rust/patches/patch-src_llvm-project_llvm_cmake_modules_AddLLVM.cmake
+++ lang/rust/patches/patch-src_llvm-project_llvm_cmake_modules_AddLLVM.cmake
@@ -1,8 +1,9 @@
-$NetBSD: patch-src_llvm-project_llvm_cmake_modules_AddLLVM.cmake,v 1.4 2020/11/13 20:35:58 he Exp $
+$NetBSD: patch-src_llvm-project_llvm_cmake_modules_AddLLVM.cmake,v 1.3 2020/08/06 11:42:56 jperkin Exp $
 
+"-z discard-unused" is only supported by Oracle Solaris ld.
 On Darwin, use correct install-name for shared libraries.
 
---- src/llvm-project/llvm/cmake/modules/AddLLVM.cmake.orig	2020-05-20 18:10:32.000000000 +0000
+--- src/llvm-project/llvm/cmake/modules/AddLLVM.cmake.orig	2020-09-22 17:14:20.000000000 +0000
 +++ src/llvm-project/llvm/cmake/modules/AddLLVM.cmake
 @@ -2063,7 +2063,7 @@ function(llvm_setup_rpath name)
    endif()
