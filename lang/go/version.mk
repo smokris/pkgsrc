ee808977e10d87d75e4f05234d7b553454cafffe
bsiegert <bsiegert@pkgsrc.org>
2021-01-23 14:07:37 +0000

    Update go115 to 1.15.7.
    * cmd/go: packages using cgo can cause arbitrary code execution at build time
    
    The go command may execute arbitrary code at build time when cgo is in use on
    Windows. This may occur when running “go get”, or any other command that builds
    code. Only users who build untrusted code (and don’t execute it) are affected.
    
    In addition to Windows users, this can also affect Unix users who have “.”
    listed explicitly in their PATH and are running “go get” or build commands
    outside of a module or with module mode disabled.
    
    Thanks to RyotaK (https://twitter.com/ryotkak) for reporting this issue.
    
    This issue is CVE-2021-3115 and Go issue golang.org/issue/43783.
    
    For more background on the cmd/go change and help deciding whether your own
    programs might have similar issues, see our blog post at
    https://blog.golang.org/path-security.
    
    * crypto/elliptic: incorrect operations on the P-224 curve
    
    The P224() Curve implementation can in rare circumstances generate incorrect
    outputs, including returning invalid points from ScalarMult.
    
    The crypto/x509 and golang.org/x/crypto/ocsp (but not crypto/tls) packages
    support P-224 ECDSA keys, but they are not supported by publicly trusted
    certificate authorities. No other standard library or golang.org/x/crypto
    package supports or uses the P-224 curve.
    
    The incorrect output was found by the elliptic-curve-differential-fuzzer
    project running on OSS-Fuzz and reported by Philippe Antoine (Catena cyber).
    
    This issue is CVE-2021-3114 and Go issue golang.org/issue/43786.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.107 2020/12/19 18:03:52 bsiegert Exp $
+# $NetBSD: version.mk,v 1.109 2021/01/23 14:07:37 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO116_VERSION=	1.16.beta1
-GO115_VERSION=	1.15.5
+GO115_VERSION=	1.15.7
 GO114_VERSION=	1.14.12
 GO113_VERSION=	1.13.15
 GO110_VERSION=	1.10.8
