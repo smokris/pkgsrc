0a02ad5291abcba0f36a531d78d8314c566d539e
bsiegert <bsiegert@pkgsrc.org>
2021-05-07 16:30:41 +0000

    Update go115 to 1.15.12.
    go1.15.11 (released 2021/04/01) includes fixes to cgo, the compiler, linker,
    runtime, the go command, and the database/sql and net/http packages. See the Go
    1.15.11 milestone on our issue tracker for details.
    
    go1.15.12 (released 2021/05/06) includes a security fix to the net/http
    package, as well as bug fixes to the runtime and the time package. See the Go
    1.15.12 milestone on our issue tracker for details.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.114 2021/03/19 17:22:54 bsiegert Exp $
+# $NetBSD: version.mk,v 1.117 2021/05/07 16:30:41 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO116_VERSION=	1.16.beta1
-GO115_VERSION=	1.15.10
+GO115_VERSION=	1.15.12
 GO114_VERSION=	1.14.12
 GO113_VERSION=	1.13.15
 GO110_VERSION=	1.10.8
